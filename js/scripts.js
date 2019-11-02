93afbb213ecca3042c16f5eecd520d4c4cfc9a50
Austin Jennings <57117233+jenniings@users.noreply.github.com>
2019-11-01 17:08:04 -1000

    added css, index, and js files to app

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+alert("Hello world");
+
+var favoriteFood = "Pizza";
+document.write(favoriteFood);
+
+// var myName = "Bob";
+// document.write(myName);
+// myName = "John Doe";
+// document.write(myName);
+
+// var simpleAddition = 2 + 2;
+// document.write(simpleAddition);
+
+// var size = 100;
+// var doubleSize = size * 2;
+// document.write(doubleSize);
+
+// var size = 100;
+// var doubleSize = size * 2;
+// var minSize = doubleSize * 2 - size / 2;
+// document.write(minSize);
