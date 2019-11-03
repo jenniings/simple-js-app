18495bd1b69fb0d5d7b9559360449ba62baf5375
Austin Jennings <57117233+jenniings@users.noreply.github.com>
2019-11-02 18:00:57 -1000

    create repository array and add starter pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,5 @@
-alert("Hello world");
-
-var favoriteFood = "Pizza";
-document.write(favoriteFood);
-
-// var myName = "Bob";
-// document.write(myName);
-// myName = "John Doe";
-// document.write(myName);
-
-// var simpleAddition = 2 + 2;
-// document.write(simpleAddition);
-
-// var size = 100;
-// var doubleSize = size * 2;
-// document.write(doubleSize);
-
-// var size = 100;
-// var doubleSize = size * 2;
-// var minSize = doubleSize * 2 - size / 2;
-// document.write(minSize);
+var repository = [
+  { name: "Bulbasaur", height: 0.7, types: ["grass"] },
+  { name: "Charmander", height: 0.6, types: ["fire"] },
+  { name: "Squirtle", height: 0.5, types: ["water"] }
+];
