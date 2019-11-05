cabd70a8994e23494e04346abe121d1884899c6b
Austin Jennings <57117233+jenniings@users.noreply.github.com>
2019-11-04 18:45:04 -1000

    add in for loop

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,17 @@ var repository = [
   { name: "Charmander", height: 0.6, types: ["fire"] },
   { name: "Squirtle", height: 0.5, types: ["water"] }
 ];
+
+for (i = 0; i < repository.length; i++) {
+  if (repository[i].height >= 0.7) {
+    document.write(repository[i].name + " (height:" + repository[i].height + ")" + " - Wow, that's big!");
+    document.write("<br>");
+  } else {
+    document.write(repository[i].name + " (height:" + repository[i].height + ")");
+    document.write("<br>");
+  }
+}
+
+// var size = 100;
+// var doubleSize = size * 2;
+// var minSize = (doubleSize * 2) - (size / 2);
