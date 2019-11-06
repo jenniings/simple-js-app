ac7ac7359bdb13ba9de87e303bc426d77f35e40e
Austin Jennings <57117233+jenniings@users.noreply.github.com>
2019-11-05 20:06:02 -1000

    create foreach function

--- js/scripts.js
+++ js/scripts.js
@@ -4,16 +4,30 @@ var repository = [
   { name: "Squirtle", height: 0.5, types: ["water"] }
 ];
 
-for (i = 0; i < repository.length; i++) {
-  if (repository[i].height >= 0.7) {
-    document.write(repository[i].name + " (height:" + repository[i].height + ")" + " - Wow, that's big!");
-    document.write("<br>");
-  } else {
-    document.write(repository[i].name + " (height:" + repository[i].height + ")");
-    document.write("<br>");
-  }
-}
+// for (i = 0; i < repository.length; i++) {
+//   if (repository[i].height >= 0.7) {
+//     document.write(repository[i].name + " (height:" + repository[i].height + ")" + " - Wow, that's big!");
+//     document.write("<br>");
+//   } else {
+//     document.write(repository[i].name + " (height:" + repository[i].height + ")");
+//     document.write("<br>");
+//   }
+// }
 
-// var size = 100;
-// var doubleSize = size * 2;
-// var minSize = (doubleSize * 2) - (size / 2);
+repository.forEach(function (item) {
+  console.log(pokemonRepository);
+  document.write(item.name + " (height: " + item.height + "m" + ") " + item.types + "<br>");
+});
+
+// function divide(dividend, divisor) {
+//   if (divisor === 0) {
+//     return "You're trying to divide by zero."
+//   }
+//   else {
+//     var result = dividend / divisor;
+//     return result;
+//   }
+// }
+
+//check in console using console.log(divide(4,2));
+
