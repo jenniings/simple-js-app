7d96e86eaa3c944684e234704aabf47772b7e052
Austin Jennings <57117233+jenniings@users.noreply.github.com>
2019-11-14 09:47:03 -1000

    modal edits

--- js/scripts.js
+++ js/scripts.js
@@ -27,12 +27,6 @@ var pokemonRepository = (function () {
     $pokemonList.appendChild($listItem)
   }
 
-  // function showDetails(item) {
-  //   pokemonRepository.loadDetails(item).then(function () {
-  //     console.log(item);
-  //   });
-  // }
-
   function loadList() {
     return fetch(apiUrl).then(function (response) {
       return response.json();
@@ -56,233 +50,155 @@ var pokemonRepository = (function () {
     }).then(function (details) {
       item.imageUrl = details.sprites.front_default;
       item.height = details.height;
+      item.weight = details.weight;
       item.types = Object.keys(details.types);
     }).catch(function (e) {
       console.error(e);
-    });
+    })
   }
 
-  // function validateEmail() {
-  //   var value = $emailInput.value;
-
-  //   if (!value) {
-  //     showErrorMessage($emailInput, 'Email is a required field.');
-  //     return false;
-  //   }
-
-  //   if (value.indexOf('@') === -1) {
-  //     showErrorMessage($emailInput, 'You must enter a valid email address.');
-  //     return false;
-  //   }
-
-  //   showErrorMessage($emailInput, null);
-  //   return true;
-  // }
-
-  // function validatePassword() {
-  //   var value = $passwordInput.value;
-
-  //   if (!value) {
-  //     showErrorMessage($passwordInput, 'Password is a required field.');
-  //     return false;
-  //   }
-
-  //   if (value.length < 8) {
-  //     showErrorMessage($passwordInput, 'The password needs to be at least 8 characters long.');
-  //     return false;
-  //   }
-
-  //   showErrorMessage($passwordInput, null);
-  //   return true;
-  // }
-
-  // function showErrorMessage($input, message) {
-  //   var $container = $input.parentElement;
-
-  //   //Remove an existing error
-  //   var error = $container.querySelector('.error-message');
-  //   if (error) {
-  //     $container.removeChild(error);
-  //   }
-
-  //   //Add the error is the message isn't empty
-  //   if (message) {
-  //     var error = document.createElement('div');
-  //     error.classList.add('error-message');
-  //     error.innerText = message;
-  //     $container.appendChild(error);
-  //   }
-  // }
-
-  // function validateForm() {
-  //   var isValidEmail = validateEmail();
-  //   var isValidPassword = validatePassword();
-  //   return isValidEmail && isValidPassword;
-
-  //   $emailInput.addEventListener('input', validateEmail);
-  //   $passwordInput.addEventListener('input', validatePassword);
-  // }
-
-  function showModal(title, text) {
-    var $modalContainer = document.querySelector('#modal-container');
-
-    //Clear all exisiting modal content
-    $modalContainer.innerHTML = '';
+  function createReusableModal() {
 
     var modal = document.createElement('div');
+    var modalElement1 = document.createElement('div');
+    var modalElement2 = document.createElement('div');
+    modalElement2.classList.add('pokemon-info')
     modal.classList.add('modal');
+    modal
 
-    //Add the new modal content
     var closeButtonElement = document.createElement('button');
     closeButtonElement.classList.add('modal-close');
     closeButtonElement.innerText = 'Close';
-    closeButtonElement.addEventListener('click, hideModal');
+    //closeButtonElement.addEventListener('click', hideModal)
 
-    var titleElement = document.createElement('h1');
-    titleElement.innerText = title;
+    var nameElement = document.createElement('h1');
+    var imageElement = document.createElement('img');
+    imageElement.classList.add('pokemon-img');
+    var heightElement = document.createElement('p');
 
-    var contentElement = document.createElement('p');
-    contentElement.innerText = text;
-
-    modal.appendChild(closeButtonElement);
-    modal.appendChild(titleElement);
-    modal.appendChild(contentElement);
+    modalElement1.appendChild(closeButtonElement);
+    modal.appendChild(modalElement1);
+    modalElement2.appendChild(nameElement);
+    modalElement2.appendChild(imageElement);
+    modalElement2.appendChild(heightElement);
+    modal.appendChild(modalElement2)
     $modalContainer.appendChild(modal);
+  }
+
+  //Function to show modal for Pokemon data
+  function showModal(item) {
+    console.log('TCL: showModal -> item', item.name.charAt(0).toUpperCase() + item.name.slice(1));
+
+    //create element for Pokemon name
+    var nameElement = document.querySelector('h1');
+    nameElement.innerText = item.name.charAt(0).toUpperCase() + item.name.slice(1);
+
+    var imageElement = document.querySelector('.pokemon-img');
+    imageElement.setAttribute('src', item.imageUrl);
+
+    var heightElement = document.querySelector('p');
+    heightElement.innerText = 'Height: ' + item.height;
 
     $modalContainer.classList.add('is-visible');
-
-    window.addEventListener('keydown', (e) => {
-      var $modalContainer = document.querySelector('#modal-container');
-      if (e.key === 'Escape' && $modalContainer.classList.contains('is-visible')) {
-        hideModal();
-      }
-    });
-
-    $modalContainer.addEventListener('click', (e) => {
-      var target = e.target;
-      if (target === $modalContainer) {
-        hideModal();
-      }
-    });
   }
 
-  document.querySelector('#show-modal').addEventListener('click', () => {
-    showModal('Modal title', 'This is the modal content!');
-  });
-
-  var dialogPromiseReject;
-
+  //Function to hide modal
   function hideModal() {
-    var $modalContainer = document.querySelector('#modal-container');
+    //var $modalContainer = document.querySelector('#modal-container');
     $modalContainer.classList.remove('is-visible');
+  }
 
-    if (dialogPromiseReject) {
-      dialogPromiseReject();
-      dialogPromiseReject = null;
+  //Function to show details of each Pokemon
+  function showDetails(item) {
+    pokemonRepository.loadDetails(item).then(function () {
+      console.log(item);
+      return item;
+    }).then(function (item) {
+      console.log('TCL: showDetails -> item', item);
+      showModal(item);
+    });
+  }
+
+  window.addEventListener('keydown', function (e) {
+    if (e.key === 'Escape' && $modalContainer.classList.contains('is-visible')) {
+      hideModal();
     }
-  }
-
-  function showDialog(title, text) {
-    showModal(title, text);
-
-    //We want to add a confirm and cancel button to the modal
-    var modal = $modalContainer.querySelector('.modal');
-
-    var confirmButton = document.createElement('button');
-    confirmButton.classList.add('modal-confirm');
-    confirmButton.innerText = 'Confirm';
-
-    var cancelButton = document.createElement('button');
-    cancelButton.classList.add('modal-cancel');
-    cancelButton.innerText = 'Cancel';
-
-    modal.appendChild(confirmButton);
-    modal.appendChild(cancelButton);
-
-    //Focus the confirmButton so that the user can simply press enter
-    confirmButton.focus();
-
-    //Return a promise that resolves when confirmed, else rejects
-    return new Promise((resolve, reject) => {
-      cancelButton.addEventListener('click', hideModal);
-      confirmButton.addEventListener('click', () => {
-        dialogPromiseReject = null; //Reset this
-        hideModal();
-        resolve();
-      });
-
-      //This can be used to reject from other functions
-      dialogPromiseReject = reject;
-    });
-  }
-
-  document.querySelector('#show-dialog').addEventListener('click', () => {
-    showDialog('Confirm action', 'Are you sure you want to do this?').then(function () {
-      alert('confirmed!');
-    }, () => {
-      alert('not confirmed');
-    });
   });
 
-  var $canvas = document.querySelector('#canvas');
-
-  var isDrawing = false;
-  var previousX = null;
-  var previousY = null;
-
-  function handleStart(e) {
-    isDrawing = true;
-
-    //Initiate previousX/previousY
-    var x = e.pageX; //X-coordinate of click/touch
-    var y = e.pageY; //Y-coordinate of click/touch
-    previousX = x;
-    previousY = y;
-  }
-
-  function handleEnd() {
-    isDrawing = false;
-  }
-
-  function handleMove(e) {
-    //To prevent drawing on hover
-    if (!isDrawing) {
-      return;
+  $modalContainer.addEventListener('click', (e) => {
+    // Since this is also triggered when clicking INSIDE the modal
+    // We only want to close if the user clicks directly on the overlay
+    var target = e.target;
+    console.log('TCL: pokemonRepository -> target', target);
+    var $modalClose = document.querySelector('.modal-close');
+    if (target === $modalContainer || $modalClose) {
+      hideModal();
     }
+  })
+  // var $canvas = document.querySelector('#canvas');
 
-    var x = e.pageX; //X-coordinate of click/touch
-    var y = e.pageY; //Y-coordinate of click/touch
+  // var isDrawing = false;
+  // var previousX = null;
+  // var previousY = null;
 
-    //This is canvas specific
-    var ctx = $canvas.getContext('2d');
+  // function handleStart(e) {
+  //   isDrawing = true;
 
-    //Draw a line from previousX/previousY to x/y
-    ctx.beginPath();
-    ctx.moveTo(previousX, previousY);
-    ctx.lineTo(x, y);
+  //   //Initiate previousX/previousY
+  //   var x = e.pageX; //X-coordinate of click/touch
+  //   var y = e.pageY; //Y-coordinate of click/touch
+  //   previousX = x;
+  //   previousY = y;
+  // }
 
-    //Set the style of the line
-    ctx.linewidth = 4;
-    ctx.strokeStyle = '#ff0000';
-    ctx.stroke();
+  // function handleEnd() {
+  //   isDrawing = false;
+  // }
 
-    //Set previous coordinates for the next move event
-    previousX = x;
-    previousY = y;
-  }
+  // function handleMove(e) {
+  //   //To prevent drawing on hover
+  //   if (!isDrawing) {
+  //     return;
+  //   }
+
+  //   var x = e.pageX; //X-coordinate of click/touch
+  //   var y = e.pageY; //Y-coordinate of click/touch
+
+  //   //This is canvas specific
+  //   var ctx = $canvas.getContext('2d');
+
+  //   //Draw a line from previousX/previousY to x/y
+  //   ctx.beginPath();
+  //   ctx.moveTo(previousX, previousY);
+  //   ctx.lineTo(x, y);
+
+  //   //Set the style of the line
+  //   ctx.linewidth = 4;
+  //   ctx.strokeStyle = '#ff0000';
+  //   ctx.stroke();
+
+  //   //Set previous coordinates for the next move event
+  //   previousX = x;
+  //   previousY = y;
+  // }
 
   return { //return both variables to pass later on
     add: add,
     getAll: getAll,
     addListItem: addListItem,
     loadList: loadList,
-    loadDetails: loadDetails
+    loadDetails: loadDetails,
+    createReusableModal: createReusableModal,
+    showModal: showModal,
+    hideModal: hideModal
   };
 })();
 
 var $pokemonList = document.querySelector('.pokemon-list'); //access pokemon list in the HTML file
 
 pokemonRepository.loadList().then(function () {
+  pokemonRepository.createReusableModal();
+
   pokemonRepository.getAll().forEach(function (pokemon) {
     pokemonRepository.addListItem(pokemon);
   });
